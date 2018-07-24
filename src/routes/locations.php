dabd45caf16a7c44510bb7f744d88b960cd552a3
quandtifier <quandtifier@gmail.com>
2018-07-24 10:58:47 -0700

    the server will not recognize posts from the timezoneapp

--- src/routes/locations.php
+++ src/routes/locations.php
@@ -29,3 +29,31 @@ $app->get('/api/locations', function (Request $request, Response $response, arra
         echo '{"error": {"text": '.$e->getMessage().'}';
     }
 });
+
+// add single location
+$app->post('/api/location/add', function (Request $request, Response $response, array $args) {
+    $formatted_address = $request->getParam('formatted_address');
+    $lat = $request->getParam('lat');
+    $lng = $request->getParam('lng');
+    echo $formatted_address;
+    $sql = "INSERT INTO location (formatted_address,lat,lng) VALUES 
+    (:formatted_address,:lat,:lng)";
+
+    try{
+        //get db obj
+        $db = new db();
+        $db = $db->connect();
+
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindParam(':formatted_address', $formatted_address);
+        $stmt->bindParam(':lat', $lat);
+        $stmt->bindParam(':lng', $lng);
+
+        $stmt->execute();
+
+        echo '{"success": {"text": "location Added"}';
+    } catch (PDOException $e) {
+        echo '{"error": {"text": '.$e->getMessage().'}';
+    }
+});
